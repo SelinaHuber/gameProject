d7fb85652a01abb36dc055b2fac985f12ed0be6a
Jan Redmann <janredmann1@gmail.com>
2021-01-27 12:31:36 +0100

    fix

--- game.js
+++ game.js
@@ -1,16 +1,15 @@
-//import Level from './Level.js';
-​
+
 let l = 1;
 let level;
 let questions;
 let score = 0;
-​
+
 function setUp() {
     level = new Level(l)
     level.setQuestions()
     questions = level.getQuestions()
 }
-​
+
 function playCard() {
     console.log(level.level, questions.length)
     let randomIndex = Math.floor(Math.random() * questions.length);
@@ -20,21 +19,21 @@ function playCard() {
     questions = questions.filter(question => question !== randomQuestion);
     if (questions.length === 0) win()
 }
-​
-function addScore (){
+
+function addScore() {
     score += level.points
 }
 function win() {
     l += 1
     setUp()
 }
-​
+
 // set up event listeners after loading the page
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     setUp()
     let cardContainer = document.getElementsByClassName('cards-container')[0].children[0]
     cardContainer.addEventListener('click', playCard);
-​
+
 })
 
 
--- questions.js
+++ questions.js
@@ -1,159 +1,181 @@
-export const questionsLevel1 = [
+const questionsLevel1 = [
     {
         question: 'What do you think I’ve been binge watching lately?',
-        type:'open question',
-        level:1
+        type: 'open question',
+        level: 1
     },
     {
         question: 'Think of the most underrated show streaming currently. On the count of three, say your answers out loud. Both players.',
-        type:'wild card',
-        level:1
+        type: 'wild card',
+        level: 1
     },
     {
         question: 'What do you think has been my go to snack during lock-down?',
-        type:'open question',
-        level:1
+        type: 'open question',
+        level: 1
     },
     {
-        question:'Share your screen time. Both players.',
-        type:'wild card',
-        level:1
+        question: 'Share your screen time. Both players.',
+        type: 'wild card',
+        level: 1
     },
     {
-        question:'What time do you think I’ve been waking up these days?',
-        type:'open question',
-        level:1
+        question: 'What time do you think I’ve been waking up these days?',
+        type: 'open question',
+        level: 1
     },
     {
-        question:'What is your number one played song at the moment?',
-        type:'open question',
-        level:1
+        question: 'What is your number one played song at the moment?',
+        type: 'open question',
+        level: 1
     },
     {
-        question:'Do you think I’m mainly cooking or postmating?',
-        type:'open question',
-        level:1
+        question: 'Do you think I’m mainly cooking or postmating?',
+        type: 'open question',
+        level: 1
     }
 ]
 
 
-export const questionsLevel2 = [
-    {   question: 'what is something new you have learned recently?',
-        type:'open question',
-        level:2
-    },
-        
-    {   question:'what are you overthinking right now?',
-        type:'open question',
-        level:2
-    }
-    ,
-    {   question:'Share your most played song at the moment.',
-        type:'wild card',
-        level:2
-    }
-    ,
-    {   question:'What is your number one played song at the moment?',
-        type:'open question',
-        level:2
-    }
-    ,
-    {   question:'What has been the kindest thing someone has done for you?',
-        type:'open question',
-        level:2
-    },
-    
-    {   question: 'What are you most excited for today?',
-        type:'open question',
-        level:2
-    },
-    
-    {   question: 'What’s the most fun you’ve had during this time?',
-        type:'open question',
-        level:2
-     },
-    
-    {   question:'What’s the most valuable lesson this time has taught you?',
-        type:'open question',
-        level:2
-    }
-    ,
-    {   question:'What movie do you which you’d never seen so you can watch it again for the first time during quarantine?',
-        type:'open question',
-        level:2
-    }
-    ,
-    {   question:'Staring contest, the one who smiles first must reveal what their kitchen looks like in this current moment. ',
-        type:'wild card',
-        level:2
-    }
-    ,
-    {   question:'When quarantining is behind us, what change do you want to make in your day to day life, if any?',
-        type:'open question',
-        level:2
+const questionsLevel2 = [
+    {
+        question: 'what is something new you have learned recently?',
+        type: 'open question',
+        level: 2
     },
 
-    {   question:'What can you create during this time with exactly what you have?',
-        type:'open question',
-        level:2
+    {
+        question: 'what are you overthinking right now?',
+        type: 'open question',
+        level: 2
+    }
+    ,
+    {
+        question: 'Share your most played song at the moment.',
+        type: 'wild card',
+        level: 2
+    }
+    ,
+    {
+        question: 'What is your number one played song at the moment?',
+        type: 'open question',
+        level: 2
+    }
+    ,
+    {
+        question: 'What has been the kindest thing someone has done for you?',
+        type: 'open question',
+        level: 2
+    },
+
+    {
+        question: 'What are you most excited for today?',
+        type: 'open question',
+        level: 2
+    },
+
+    {
+        question: 'What’s the most fun you’ve had during this time?',
+        type: 'open question',
+        level: 2
+    },
+
+    {
+        question: 'What’s the most valuable lesson this time has taught you?',
+        type: 'open question',
+        level: 2
+    }
+    ,
+    {
+        question: 'What movie do you which you’d never seen so you can watch it again for the first time during quarantine?',
+        type: 'open question',
+        level: 2
+    }
+    ,
+    {
+        question: 'Staring contest, the one who smiles first must reveal what their kitchen looks like in this current moment. ',
+        type: 'wild card',
+        level: 2
+    }
+    ,
+    {
+        question: 'When quarantining is behind us, what change do you want to make in your day to day life, if any?',
+        type: 'open question',
+        level: 2
+    },
+
+    {
+        question: 'What can you create during this time with exactly what you have?',
+        type: 'open question',
+        level: 2
     }
 ]
 
 
-export const questionsLevel3 = [
-    {   question: 'How has your view on family evolved over time, if at all?',
-        type:'open question',
-        level:3
+const questionsLevel3 = [
+    {
+        question: 'How has your view on family evolved over time, if at all?',
+        type: 'open question',
+        level: 3
     },
-        
-    {   question:'What about you has felt repeatedly misunderstood by others, if anything?',
-        type:'open question',
-        level:3
+
+    {
+        question: 'What about you has felt repeatedly misunderstood by others, if anything?',
+        type: 'open question',
+        level: 3
     }
     ,
-    {   question:'What did the people who raised you taught you the most?',
-        type:'open question',
-        level:3
+    {
+        question: 'What did the people who raised you taught you the most?',
+        type: 'open question',
+        level: 3
     },
     {
-        question:'What part of your job energizes you the most? Leaves you the most drained?',
-        type:'open question',
-        level:3
-    },
-    
-    {   question: 'What are you most excited foWhat has being in the Bootcamp taught you about yourself?',
-        type:'open question',
-        level:3
-    },
-    
-    {   question: 'What advice would you give your younger self?',
-        type:'open question',
-        level:3
-     },
-    
-    {   question:'Which one of your parents’ personality traits do you want to keep? Let go of?',
-        type:'open question',
-        level:3
+        question: 'What part of your job energizes you the most? Leaves you the most drained?',
+        type: 'open question',
+        level: 3
     },
 
-    {   question: 'Write down 3 things you are grateful for during this time. ',
-        type:'wild card',
-        level:3
-     }
+    {
+        question: 'What are you most excited foWhat has being in the Bootcamp taught you about yourself?',
+        type: 'open question',
+        level: 3
+    },
+
+    {
+        question: 'What advice would you give your younger self?',
+        type: 'open question',
+        level: 3
+    },
+
+    {
+        question: 'Which one of your parents’ personality traits do you want to keep? Let go of?',
+        type: 'open question',
+        level: 3
+    },
+
+    {
+        question: 'Write down 3 things you are grateful for during this time. ',
+        type: 'wild card',
+        level: 3
+    }
     ,
-    {   question:'What movie do you which you’d never seen so you can watch it again for the first time during quarantine?',
-        type:'open question',
-        level:3
-    },
-    
-    {   question: 'Write down a short definition of happiness for you personally. Both players.',
-        type:'wild card',
-        level:3
+    {
+        question: 'What movie do you which you’d never seen so you can watch it again for the first time during quarantine?',
+        type: 'open question',
+        level: 3
     },
 
-    {   question:'What can you create during this time with exactly what you have?',
-        type:'open question',
-        level:3
+    {
+        question: 'Write down a short definition of happiness for you personally. Both players.',
+        type: 'wild card',
+        level: 3
+    },
+
+    {
+        question: 'What can you create during this time with exactly what you have?',
+        type: 'open question',
+        level: 3
     }
 ]
 
