b43ceda775f37fc1f0832aa248b240b0aca2c46b
Selina <Selina.Huber@gmx.com>
2021-01-27 11:00:24 +0100

    old Version

--- game.js
+++ game.js
@@ -14,22 +14,23 @@ class Level {
 }
 
 class Card {
-    constructor (question, questionType) {
+    constructor (question, questionType, level) {
         this.question=question;
         this.questionType=questionType;
+        this.level=level;
     }   
 }
 
 const cardLevel1 = questionsLevel1.map((question) => {
-    return new Card(question.question, question.questionType)
+    return new Card(question.question, question.questionType, question.level)
 });
 
 const cardLevel2 = questionsLevel2.map((question) => {
-    return new Card(question.question, question.questionType)
+    return new Card(question.question, question.questionType, question.level)
 });
 
 const cardLevel3 = questionsLevel3.map((question) => {
-    return new Card(question.question, question.questionType)
+    return new Card(question.question, question.questionType, question.level)
 });
 
 const level1 = new Level(1, 100, cardLevel1);
@@ -50,6 +51,7 @@ const level3 = new Level(3, 300, cardLevel3);
         console.log (cardLevel[i].question)
     }
 } */
+
 
 
 
@@ -76,7 +78,11 @@ function setCardDeck () {
         console.log ('Level 3 length is', cardLevel3.length) */
 }
 
-
+function alertGameOver() {
+    if(confirm('You have gone through all the cards. You have to restart the game.')){
+        window.location.reload();  
+    };
+} 
 
 /* // shuffle the card game
 function shuffleArray(array) {
@@ -99,29 +105,60 @@ let cardsContainer = document.getElementsByClassName('cards-container')[0].child
 cardsContainer.addEventListener('click', playCard);
 
 
+/* function showArray(){
+    for (var i = 0; i < arr.length; i++){
+        console.log((i + 1) + '. ' + arr[i])
+    }
+} */
+
 function playCard() {
     setCardDeck()
-    let randomIndex = Math.floor(Math.random() * questions.length);
+        let randomIndex = Math.floor(Math.random() * questions.length);
     let randomQuestion = questions[randomIndex];
     // what happends if it is already there. Then I would have to click again
-    while (!playedCards.includes(randomQuestion)) {
+    if (!playedCards.includes(randomQuestion)) {
         let questionDiv = document.getElementById('question');
         questionDiv.innerHTML = randomQuestion.question;
         questions.splice(randomIndex, 1)
         playedCards.push(randomQuestion);
+        addScore();
     }
+    
 }
-
-
 
 // while (playedCards.every(randomQuestion)) {
 
-function alertGameOver() {
-    if(confirm('You have gone through all the cards. You have to restart the game.')){
-        window.location.reload();  
-    };
-} 
+ // function to add score 
+ // loop over playedCards
+ // for Each questions added of level 1 add 100 points to score
+ // for Each questions addes of level 2 add 200 ponts
+
+function addScore(){
+    let score = 0;
+    if (playedCards.length === 0) return 0;
+    for (var i = 0; i < playedCards.length; i++){
+        if (playedCards[i].level === 1) {
+            score += 100
+        } else if (playedCards[i].level === 2){
+            score += 200
+        } else if (playedCards[i].level === 3){
+            score += 300
+        }
+    }
+    let scoreDiv = document.getElementById('score');
+    scoreDiv.innerText = `Connection Score : ${score}`
+    console.log (score)
+}
+    
+    //const scoreNo = playedCards.map(function (usedQuestion){
+    
 
 
+// print all questions seperately
+/* for (var i = 0; i < cardLevel3.length; i++){
+    console.log((i + 1) + '. ' + cardLevel3[i].question)
+} */
+
+console.log (playedCards)
 
 
